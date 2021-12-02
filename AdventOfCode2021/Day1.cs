57b392489ae641c1c7f45a960086fff43dd50738
Tom Haumersen <tbhaum@gmail.com>
2021-12-02 18:17:15 -0500

    add first 2021 problem

---
+++ AdventOfCode2021/Day1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    internal class Day1
+    {
+        private static int SET_SIZE = 3;
+        internal static void Run()
+        {
+            var depths = input.day1.Split("\r\n").Select(x => int.Parse(x)).ToArray();
+            int increadingCount = 0;
+            for(int i = 1; i < depths.Length; i++)
+            {
+                if (depths[i] > depths[i - 1])
+                    increadingCount++;
+            }
+            Console.WriteLine($"Day 1 - Part 1: {increadingCount} increasing scans");
+
+            int setSize = 3;
+            int windowA = 0;
+            int windowB = 0;
+            increadingCount = 0;
+
+            windowA = calculateWindowSum(depths, 0);
+
+            for (int i = 1; i < depths.Length - SET_SIZE + 1; i++)
+            {
+                windowB = getNextWindowSum(depths, i, windowA);
+                if (windowB > windowA)
+                    increadingCount++;
+                windowA = windowB;
+            }
+            Console.WriteLine($"Day 1 - Part 2: {increadingCount} increasing windows");
+        }
+
+        private static int calculateWindowSum(int[] depths, int index)
+        {
+            var windowSum = 0;
+            for(int i = 0; i < SET_SIZE; i++)
+            {
+                windowSum += depths[index + i];
+            }
+            return windowSum;
+        }
+
+        private static int getNextWindowSum(int[] depths, int index, int previousWindowSum)
+        {
+            return previousWindowSum - depths[index - 1] + depths[index + SET_SIZE-1];
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2021;
+
+Console.WriteLine("Hello, World!");
+Day1.Run();
