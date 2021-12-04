f3d5875ae04bd01eb9c2d45d77e55abf2a5c2527
Tom Haumersen <tbhaum@gmail.com>
2021-12-03 22:25:58 -0500

    finish day3 part 2

--- AdventOfCode2021/Day3.cs
+++ AdventOfCode2021/Day3.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,11 +28,61 @@ namespace AdventOfCode2021
                 }
             }
 
-            Console.WriteLine(string.Join(' ', accum));
+            Debug.WriteLine(string.Join(' ', accum));
             var gamma = accum.Select(x => x > lines.Length/2).Aggregate(0, (acc, x) => (acc << 1 )+ (x ?  1 : 0));
             var mask = Enumerable.Range(0, LINE_WIDTH).Aggregate(0, (acc, x) => (acc << 1) + 1);
             var epsilon = gamma ^ mask;
+
             Console.WriteLine($"Day 3 - Part 1: gamma [{gamma}] | epsilon [{epsilon}] | product [{gamma * epsilon}]");
+
+            var oxygenGenRating = GetRating(lines, true);
+            var co2ScrubberRating = GetRating(lines, false);
+
+            Console.WriteLine($"Day 3 - Part 2: {nameof(oxygenGenRating)} [{oxygenGenRating}] | {nameof(co2ScrubberRating)} [{co2ScrubberRating}] | product [{oxygenGenRating * co2ScrubberRating}]");
+        }
+        private static int GetRating(string[] lines, bool lookForOneOnMoreOnes)
+        {
+            var acceptedLeft = lines.Length;
+            var accepted = new BitArray(lines.Length, true);
+            var ratingStr = string.Empty;
+            var lookForOnMoreOnes = lookForOneOnMoreOnes ? '1' : '0';
+            var lookForOnMoreZeros = lookForOneOnMoreOnes ? '0' : '1';
+
+            for (int bitIndex = 0; bitIndex < LINE_WIDTH; bitIndex++)
+            {
+                var onesCount = 0;
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (!accepted[i])
+                        continue;
+                    onesCount += lines[i][bitIndex] == '1' ? 1 : 0;
+                }
+
+                var zerosCount = (acceptedLeft - onesCount);
+                var lookFor = onesCount >= zerosCount ? lookForOnMoreOnes : lookForOnMoreZeros;
+                Debug.WriteLine(onesCount + " - " +  lookFor);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (!accepted[i])
+                        continue;
+                    if (lines[i][bitIndex] != lookFor)
+                    {
+                        accepted[i] = false;
+                        acceptedLeft--;
+                        continue;
+                    }
+                    Debug.WriteLine(lines[i]);
+                    ratingStr = lines[i];
+                }
+                Debug.WriteLine("");
+                if (acceptedLeft == 1)
+                    break;
+            }
+            var rating = Convert.ToInt32(ratingStr, 2);
+            Debug.WriteLine($"rating [{rating}]");
+            return rating;
         }
     }
 }
